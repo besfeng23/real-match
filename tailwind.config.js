e7aacdc84f770d324a061b068eda9dbb14118e26
besfeng23 <136943241+besfeng23@users.noreply.github.com>
2026-05-28 22:52:18 +0800

    chore: add Tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  content: ["./app/page.tsx", "./app/layout.tsx", "./components/SiteHeader.tsx", "./components/SiteFooter.tsx"],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
